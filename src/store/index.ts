07cf3a2fd3d27d98ade4cda244c0be90c05adcce
yaominguo <missgmy@yahoo.com>
2021-06-17 10:41:02 +0800

    trans auth-context to async redux(with redux thunk)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { listSlice } from "screens/project-list/list-slice";
+import { authSlice } from "./auth-slice";
 
 export const rootReducer = {
   projectList: listSlice.reducer,
+  auth: authSlice.reducer,
 };
 
 export const store = configureStore({
